ba808936a462e91a3492628141232865599f97d4
Shashwat Swain <shashwatswainofficial@gmail.com>
2018-12-15 08:04:15 +0530

    added index.html

--- app.py
+++ app.py
@@ -63,5 +63,77 @@ def done():
     
     redir = redirect_url()
 
-    return redirect_url
+    return redirect(redir)
+
+@app.route("/action", methods=['POST'])
+def action():
+    """Adding a Task"""
+    name = request.values.get("name")
+    desc = request.values.get("desc")
+    date = request.values.get("date")
+    pr = request.values.get("pr")
+    todos.insert({
+                "name" : name,
+                "desc" : desc,
+                "date" : date,
+                "pr" : pr,
+                "done" : "no"})
+
+@app.route("/remove")
+def remove():
+    """Deleting a Task"""
+    key = request.values.get("_id")
+    todos.remove({"_id" : ObjectId(key)})
+
+    return redirect("/")
+
+@app.route("/update")
+def update():
+    id = request.values.get("_id")
+    task = todos.find({"_id" : ObjectId(id)})
     
+    return render_template('update.html',
+                            tasks=task,
+                            h=heading,
+                            t=title)
+
+@app.route("/action2",methods=['POST'])
+def action2():
+    """Updating a Task with various references"""
+    name = request.values.get("name")
+    desc = request.values.get("desc")
+    date = request.values.get("date")
+    pr = request.values.get("_id")
+
+    todos.update({
+        {"_id" : ObjectId(id)},
+        {'$set': {
+                    "name":name,
+                    "desc":desc,
+                    "date":date,
+                    "pr":pr
+                  }}
+        })
+
+    return redirect("/")
+
+@app.route("/search", methods=['GET'])
+def search():
+    """Searching a Task with a reference"""
+
+    key = request.values.get("key")
+    refer = request.values.get("refer")
+    if key=="_id":
+        todos_l = todos.find(
+            {refer : ObjectId(key)})
+    else:
+        todos_l = todos.find({refer: key})
+
+    return render_template("searchlist.html",
+                            todos=todos_l,
+                            t=title,
+                            h=heading)
+
+if __name__ == "__main__":
+
+    app.run()
